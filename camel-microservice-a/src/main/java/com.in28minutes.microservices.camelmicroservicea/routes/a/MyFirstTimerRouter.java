337111f60421fd31d264e50a68bceb45df90b6a6
Renato Veneroso <rveneroso@gmail.com>
2026-03-31 18:13:04 -0300

    Código concluído até o término da aula 5

--- camel-microservice-a/src/main/java/com.in28minutes.microservices.camelmicroservicea/routes/a/MyFirstTimerRouter.java
+++ camel-microservice-a/src/main/java/com.in28minutes.microservices.camelmicroservicea/routes/a/MyFirstTimerRouter.java
@@ -1,12 +1,22 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.a;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class MyFirstTimerRouter extends RouteBuilder {
+
+    @Autowired
+    private GetCurrentTimeBean getCurrentTimeBean;
+
+    @Autowired
+    private SimpleLoggingProcessingComponent loggingComponent;
 
     @Override
     public void configure() throws Exception {
@@ -16,12 +26,42 @@ public class MyFirstTimerRouter extends RouteBuilder {
 
         // Nos primeiros estágios do curso serão usados um timer como entrada e log como saída.
         // Por padrão, o timer executa de 1 em 1 segundo
+
+        // Essa nova versão adiciona a saída gerada pelo método getCurrentTime do bean GetCurrentTimeBean ao body da mensagem.
+        // O Camel usa automaticamente o retorno do bean como corpo da mensagem (body) — e é isso que o log está imprimindo.
+        // O bean GetCurrentTimeBean tem apenas um método público. Por isso o Camel o utiliza como default.
+        // É possível também especificar qual método deve ser chamado para compor o body da mensagem: bean("getCurrentTimeBean", "getCurrentTime").
+
+        // No Apache Camel, processing é qualquer processamento que seja faça utilizando a mensagem recebida SEM ALTERÁ-LA. Já o Transformation,
+        // como o próprio nome indica, é o processamento que modifica a mensagem recebida.
+
+        // O corpo da mensagem pode ser acessado com {$body}
         from("timer:first-timer") // A palavra timer define o tipo de endpoint. O nome first-timer é de escolha do desenvolvedor
-                .transform().constant("Time now is " + LocalDateTime.now()) // Adiciona a data e hora atuais à mensagem original recebida pelo timer
+                .log("${body}")
+                .bean(getCurrentTimeBean)
+                .log("${body}")
+                .bean(loggingComponent)
+                .log("${body}")
                 .to("log:first-timer"); // O mesmo que acima: log é o tipo de endpoint, first-time é o nome dado a ele pelo desenvolvedor.
 
-        // Sem uma implementação correta da mensagem, os logs monstram ininterruptamente a mensagem abaixo:
-        // Exchange[ExchangePattern: InOnly, BodyType: null, Body: [Body is null]]
+    }
+}
 
+@Component
+class GetCurrentTimeBean {
+    public String getCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+        return now.format(formatter);
+    }
+}
+
+@Component
+class SimpleLoggingProcessingComponent {
+    Logger logger = LoggerFactory.getLogger(SimpleLoggingProcessingComponent.class);
+    public void process(String message) {
+        logger.info("SimpleLoggingProcessingComponent {}", message); //
     }
 }
