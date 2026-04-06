9cb5a648516f3181f6a152fd561c7980734e6337
Renato Veneroso <rveneroso@gmail.com>
2026-04-06 11:00:35 -0300

    Código da aula 26 - Dynamic routing.

--- camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -1,22 +1,21 @@
 package com.in28minutes.microservices.camelmicroservicea.routes.patterns;
 
-import com.in28minutes.microservices.camelmicroservicea.CurrencyExchange;
+import org.apache.camel.ExchangeProperties;
+import org.apache.camel.Headers;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 
 @Component
 public class EipPatternsRouter extends RouteBuilder {
 
-    private SplitterComponent splitterComponent;
+    private DynamicRouterBean dynamicRouterBean;
 
-    public EipPatternsRouter(SplitterComponent splitterComponent) {
-        this.splitterComponent = splitterComponent;
+    public EipPatternsRouter(DynamicRouterBean dynamicRouterBean) {
+        this.dynamicRouterBean = dynamicRouterBean;
     }
 
     @Override
@@ -24,30 +23,20 @@ public class EipPatternsRouter extends RouteBuilder {
 
         Logger logger = LoggerFactory.getLogger(EipPatternsRouter.class);
 
-        String dynamicRoutingSlip = "direct:endpoint1,direct:endpoint2,direct:i-can-put-any-name-here";
 
         from("timer:routingSlip?period=10000")
                 .transform().constant("This message is hardcoded, something that you definitely should avoid")
-                .routingSlip(simple(dynamicRoutingSlip));
+                .dynamicRouter(method(dynamicRouterBean));
 
-        from("direct:endpoint1")
-                .to("log:this-is-endpoint1");
-
-        from("direct:endpoint2")
-                .to("log:this-is-endpoint2");
-
-        from("direct:i-can-put-any-name-here")
-                .log("Running i-can-put-any-name-here endpoint")
-                .to("activemq:routing-slip-queue");
     }
 }
 
 @Component
-class SplitterComponent {
+class DynamicRouterBean {
 
-    Logger logger = LoggerFactory.getLogger(SplitterComponent.class);
-    public List<String> splitInput(String body, String delimiter) {
-        logger.info("CVS file received: {}", body);
-        return Arrays.asList(body.split(delimiter));
+    Logger logger = LoggerFactory.getLogger(DynamicRouterBean.class);
+    public String decideNextEndpoint(@ExchangeProperties Map<String, String> properties, @Headers Map<String, String> headers, String body) {
+        logger.info("Properties: {}, Headers: {}, Body: {}", properties, headers, body);
+        return "activemq:dynamic-router-queue";
     }
 }
