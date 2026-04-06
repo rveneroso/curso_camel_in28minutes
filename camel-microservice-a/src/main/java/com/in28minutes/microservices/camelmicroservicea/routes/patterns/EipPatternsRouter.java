3c402cd4831766eb84ad6950b14e5811870061ec
Renato Veneroso <rveneroso@gmail.com>
2026-04-06 15:29:46 -0300

    Código das aulas 27 e 28 - Dynamic configuration e Trace - DLQ - WireTap

--- camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
+++ camel-microservice-a/src/main/java/com/in28minutes/microservices/camelmicroservicea/routes/patterns/EipPatternsRouter.java
@@ -19,17 +19,23 @@ public class EipPatternsRouter extends RouteBuilder {
         this.dynamicRouterBean = dynamicRouterBean;
     }
 
-
-
-
     @Override
     public void configure() throws Exception {
 
         Logger logger = LoggerFactory.getLogger(EipPatternsRouter.class);
 
+        // Tracing eleva ainda mais o nível de informações que o Camel loga durante as ações, independente do nível de log definido para a aplicação.
+        getContext().setTracing(true);
+
+        // Em caso de erro, é possível direcionar as mensagens que falharam para uma DLQ - Dead letter queue
+        errorHandler(deadLetterChannel("activemq:general-dead-letter-queue"));
 
         from("timer:routingSlip?period=10000")
                 .transform().constant("This message is hardcoded, something that you definitely should avoid")
+                // Camel também oferece um recurso para o envio da mensagem para um endpoint alternativo. Esse envio é assíncrono e não afeta nem o fluxo nem a mensagem original.
+                // É um recurso útil principalmente para auditoria. No exemplo abaixo, além da mensagem ser enviada ao endpoint definido dinamicamente, uma cópia da mensagem será enviada à fila audit-queue no ActiveMQ.
+                // Importante: wireTap só executa 1 vez para cada Exchange.
+                .wireTap("activemq:audit-queue")
                 .dynamicRouter(method(dynamicRouterBean));
 
     }
@@ -40,23 +46,21 @@ class DynamicRouterBean {
 
     Logger logger = LoggerFactory.getLogger(DynamicRouterBean.class);
 
-
-    @Value("${app.dynamic-routes.even}")
-    private String evenRoute;
-
-    @Value("${app.dynamic-routes.odd}")
-    private String oddRoute;
-
     private int invocations;
 
     public String decideNextEndpoint(@ExchangeProperties Map<String, String> properties, @Headers Map<String, String> headers, String body) {
+
         logger.info("Properties: {}, Headers: {}, Body: {}", properties, headers, body);
         invocations++;
 
+        if(invocations>10) return null;
+
+        // O Camel tem uma sintaxe própria que permite ler diretatmente os valores das propriedades definidas em arquivos yaml, sem a necessidade de usar ConfigurationProperties ou anotações @value.
+        // Basta usar a sintaxe {{}} como no código abaixo.
         if(invocations%2==0) {
-            return evenRoute;
+            return "{{app.dynamic-routes.even}}";
         } else {
-            return oddRoute;
+            return "{{app.dynamic-routes.odd}}";
         }
     }
 }
